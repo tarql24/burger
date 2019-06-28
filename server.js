e73d8b27b152f913b7a59e3d0927a354e431e494
tarql24 <tarql24@gmail.com>
2019-06-27 18:32:10 -0700

    creted server file

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+// ==============================================================================
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+// app.use(express.static('app'));
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(PORT, function() {
+  console.log('App listening on PORT: ' + PORT);
+});
