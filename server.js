82561896ec4ca9c10a5986c72e3054d7793f8623
tarql24 <tarql24@gmail.com>
2019-07-29 17:28:13 -0700

    added mysql db

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
-  console.log('Server listening on: http://localhost:' + PORT);
+  console.log('server listening on: http://localhost:' + PORT);
 });
 
 //some code
